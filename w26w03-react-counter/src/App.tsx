c20b30820d8b4cc1be478eb96538c431e5203cff
Sia-my <rlat1204@gmail.com>
2026-09-17 19:45:58 +0900

    first commit

---
+++ w26w03-react-counter/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={ () => {
+        setCount(count + 1)
+        setCount(count + 1)
+        setCount(count + 1)
+        setCount(count + 1)
+        setCount(count + 1)
+      } }>
+        증가
+      </button>
+    </>
+  )
+}
+
+export default App
